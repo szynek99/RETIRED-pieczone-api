38620cd8d98093f154d364b7bf273ead17ed9d16
szynek99 <pielat.szymon@gmail.com>
2023-10-09 17:05:20 +0200

    config: adjustments

--- src/db/config.js
+++ src/db/config.js
@@ -15,9 +15,9 @@ module.exports = {
   test: {
     host: '127.0.0.1',
     dialect: 'postgres',
-    database: process.env.DB_NAME,
+    database: 'pieczone-test',
     port: '5433',
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    username: 'postgres',
+    password: 'password',
   },
 };
